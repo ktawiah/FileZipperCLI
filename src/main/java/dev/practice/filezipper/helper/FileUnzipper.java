d3f0a4302d81ac08b3f11d552644fbd81fd5fbb5
Kelvin <kelvintawiah224@gmail.com>
2025-02-17 04:55:17 -0600

    add helper zip and unzip classes for files and directory

---
+++ src/main/java/dev/practice/filezipper/helper/FileUnzipper.java
@@ -0,0 +1,73 @@
+package dev.practice.filezipper.helper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class FileUnzipper {
+
+    /**
+     * Extracts the content of a zip file into the specified destination directory.
+     * @param zipFilePath The path to the zip file.
+     * @param destDirPath The directory where the zip content will be extracted.
+     * @throws IOException If an I/O error occurs during extraction.
+     */
+    public void unzip(String zipFilePath, String destDirPath) throws IOException {
+        File destDir = new File(destDirPath);
+        if (!destDir.exists()) {
+            destDir.mkdirs();
+        }
+
+        try (FileInputStream fis = new FileInputStream(zipFilePath);
+             ZipInputStream zis = new ZipInputStream(fis)) {
+
+            ZipEntry zipEntry;
+            while ((zipEntry = zis.getNextEntry()) != null) {
+                File extractedFile = new File(destDir, zipEntry.getName());
+
+                if (zipEntry.isDirectory()) {
+                    createDirectory(extractedFile);
+                } else {
+                    extractFile(zis, extractedFile);
+                }
+
+                zis.closeEntry();
+            }
+        }
+    }
+
+    /**
+     * Creates a directory if it doesn't already exist.
+     * @param dir The directory to be created.
+     */
+    private void createDirectory(File dir) {
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+    }
+
+    /**
+     * Extracts a single file from the zip archive.
+     * @param zis The ZipInputStream to read the file from.
+     * @param file The file to be extracted.
+     * @throws IOException If an I/O error occurs while writing the file.
+     */
+    private void extractFile(ZipInputStream zis, File file) throws IOException {
+        // Ensure the parent directories exist
+        File parentDir = file.getParentFile();
+        if (!parentDir.exists()) {
+            parentDir.mkdirs();
+        }
+
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = zis.read(buffer)) != -1) {
+                fos.write(buffer, 0, bytesRead);
+            }
+        }
+    }
+}
---
+++ src/main/java/dev/practice/filezipper/helper/FileZipper.java
@@ -0,0 +1,85 @@
+package dev.practice.filezipper.helper;
+
+import java.io.*;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class FileZipper {
+
+    /**
+     * Compresses the given file or directory into a zip archive.
+     * @param sourcePath The path of the file or directory to be zipped.
+     * @param zipFileName The name of the output zip file (without the ".zip" extension).
+     * @throws IOException If an I/O error occurs during zipping.
+     */
+    public void zip(String sourcePath, String zipFileName) throws IOException {
+        File sourceFile = new File(sourcePath);
+
+        if (!sourceFile.exists()) {
+            throw new FileNotFoundException("Source not found: " + sourcePath);
+        }
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(zipFileName + ".zip");
+             ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream)) {
+
+            zipRecursively(sourceFile, sourceFile.getName(), zipOutputStream);
+        }
+    }
+
+    /**
+     * Recursively zips files and directories.
+     * @param fileOrDir The file or directory to be zipped.
+     * @param zipEntryName The name of the current entry in the zip file.
+     * @param zos The ZipOutputStream to write entries to.
+     * @throws IOException If an I/O error occurs while reading files or writing to the zip stream.
+     */
+    private void zipRecursively(File fileOrDir, String zipEntryName, ZipOutputStream zos) throws IOException {
+        if (fileOrDir.isDirectory()) {
+            zipDirectory(fileOrDir, zipEntryName, zos);
+        } else {
+            zipFile(fileOrDir, zipEntryName, zos);
+        }
+    }
+
+    /**
+     * Adds a directory and its contents to the zip archive.
+     * @param dir The directory to be zipped.
+     * @param parentDir The name of the parent directory in the zip file.
+     * @param zos The ZipOutputStream to write the directory and its contents to.
+     * @throws IOException If an I/O error occurs.
+     */
+    private void zipDirectory(File dir, String parentDir, ZipOutputStream zos) throws IOException {
+        // Add a trailing '/' to the zip entry for directories
+        if (Objects.requireNonNull(dir.listFiles()).length == 0) {
+            zos.putNextEntry(new ZipEntry(parentDir + "/"));
+            zos.closeEntry(); // Close empty directory entry
+        } else {
+            for (File file : Objects.requireNonNull(dir.listFiles())) {
+                zipRecursively(file, parentDir + "/" + file.getName(), zos);
+            }
+        }
+    }
+
+    /**
+     * Adds a single file to the zip archive.
+     * @param file The file to be zipped.
+     * @param zipEntryName The name of the file in the zip archive.
+     * @param zos The ZipOutputStream to write the file entry to.
+     * @throws IOException If an I/O error occurs while reading the file or writing to the zip stream.
+     */
+    private void zipFile(File file, String zipEntryName, ZipOutputStream zos) throws IOException {
+        try (FileInputStream fis = new FileInputStream(file)) {
+            ZipEntry zipEntry = new ZipEntry(zipEntryName);
+            zos.putNextEntry(zipEntry);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                zos.write(buffer, 0, bytesRead);
+            }
+
+            zos.closeEntry();
+        }
+    }
+}
