8317c538e2216b4eb3b5b79c28b641ed06b9ad23
Kelvin <kelvintawiah224@gmail.com>
2025-02-17 01:45:48 -0600

    setup project

---
+++ src/main/java/dev/practice/filezipper/FileZipperApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.filezipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileZipperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileZipperApplication.class, args);
+    }
+
+}
